13c5459e32645ef037ee76db7acb46e26c018b29
Christian Velin <christian@dotslashme.com>
2018-01-04 19:12:02 +0100

    Created ip file in tmp, but implementation remain. Proper parsing of result

--- noipy.py
+++ noipy.py
@@ -1,26 +1,26 @@
 import re
 import requests
+import os
+from datetime import datetime, timedelta
+
 
 ''' Left to do:
         - implement logging
         - implement tmp file to avoid unneccessary api updates
-        - Proper parsing of result
 '''
 
 noip_username = ''  # Your no-ip.com username/email
 noip_password = ''  # Your no-ip.com password
-host_names = []  # The host or hosts to update
-
-api_url = 'https://dynupdate.no-ip.com/nic/update'
-client_agent_name = 'NoIpy ddns update client'
-client_agent_version = '0.0.1'
-client_agent_maintainer = 'christian@dotslashme.com'
-ip_resolvers = [
-    'http://icanhazip.com/'
-]
+host_names = ['']  # The host or hosts to update
 
 
 def get_external_ip():
+    '''Get the external ip of the client'''
+
+    ip_resolvers = [
+        'http://icanhazip.com/'
+    ]
+
     for resolver in ip_resolvers:
         try:
             response = requests.get(resolver)
@@ -41,14 +41,12 @@ def parse_data_to_ip(data):
 
 
 def update_api(ip):
-    user_agent = client_agent_name
-    + '/'
-    + client_agent_version
-    + ' '
-    + client_agent_maintainer
+    api_url = 'https://dynupdate.no-ip.com/nic/update'
+    user_agent = 'NoIpy ddns update client/0.0.1 christian@dotslashme.com'
 
     headers = {'user-agent': user_agent}
     payload = {'myip': ip, 'hostname': host_names}
+
     try:
         response = requests.get(
             api_url,
@@ -59,11 +57,76 @@ def update_api(ip):
     except requests.exceptions.RequestException as e:
         print(e)
     else:
-        print(response.url)
-        print(response.status_code)
-        print(response.text)
+        return response.text
 
 
-data = get_external_ip()
-ip = parse_data_to_ip(data)
-update_api(ip)
+def check_response(response, ip):
+    if ip in response:
+        process_success(response, ip)
+    else:
+        process_error(response)
+
+
+def process_success(response, ip):
+    fp = open('/tmp/ip.noipy', 'w')
+    fp.write(ip)
+    fp.close()
+    print('The job completed successfully')
+
+
+def process_error(response):
+    if 'nohost' in response:
+        print('There is no DNS record for one or more hostnames. Check config')
+    elif 'badauth' in response:
+        print('The supplied credentials seems to be invalid. Check config')
+    elif 'badagent' in response:
+        print('This update agent has been banned, setting up quarantine')
+        quarantine_client()
+    elif '!donator' in response:
+        print('Update operation not supported by your subscription')
+    elif 'abuse' in response:
+        print('Abuse reported for this user, setting up quarantine')
+        quarantine_client()
+    else:  # 911
+        print('The noip server has issues, setting temporary quarantine')
+        now = datetime.now()
+        quarantined_until = now + timedelta(minutes=45)
+        quarantine_client(str(quarantined_until))
+
+
+def is_quarantined():
+    quarantine_file = '/tmp/quarantined.noipy'
+
+    if os.path.isfile(quarantine_file):
+        if os.stat(quarantine_file).st_size == 0:
+            return True
+        else:
+            fh = open(quarantine_file, 'r')
+            data = fh.read()
+            now = datetime.now()
+            quarantined_until = datetime.strptime(data, '%Y-%m-%d %H:%M:%S.%f')
+
+            if quarantined_until < now:
+                fh.close()
+                os.remove(quarantine_file)
+                return False
+            else:
+                fh.close()
+                return True
+
+
+def quarantine_client(time=None):
+    fp = open('/tmp/quarantined.noipy', 'w')
+    if time is not None:
+        fp.write(time)
+    fp.close()
+
+
+if __name__ == '__main__':
+    if is_quarantined():
+        print('This client has been quarantined - exiting')
+    else:
+        data = get_external_ip()
+        ip = parse_data_to_ip(data)
+        response = update_api(ip)
+        check_response(response, ip)
